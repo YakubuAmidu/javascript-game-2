6d2cfeaa7f38231f8d583f4f8b87ac5e543fea4d
Yakubuamidu <yakubuamidu577@gmail.com>
2023-07-13 08:30:11 -0700

    Initial commit

---
+++ player.js
@@ -0,0 +1,17 @@
+class Player {
+    constructor(game){
+        this.game = game;
+        this.width = 100;
+        this.height = 91.3;
+        this.x = 0;
+        this.y = 100;
+    }
+
+    update(){
+
+    }
+
+    draw(context){
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
